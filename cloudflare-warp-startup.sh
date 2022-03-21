1b0b42ee82ac941b6b12aede14f13a32bf6ec1db
Aofei Sheng <aofei@aofeisheng.com>
2022-03-21 15:28:15 +0800

    refactor: enable always-on after registration

--- cloudflare-warp-startup.sh
+++ cloudflare-warp-startup.sh
@@ -3,7 +3,10 @@
 PRESTART_SCRIPT=/usr/lib/supervisor/scripts/cloudflare-warp-prestart.sh
 if [[ -x "$PRESTART_SCRIPT" ]]; then $PRESTART_SCRIPT; fi
 /usr/bin/supervisorctl start cloudflare-warp-daemon
-if [[ "$(/usr/bin/warp-cli --accept-tos account)" == *"Missing"* ]]; then /usr/bin/warp-cli --accept-tos register; fi
+if [[ "$(/usr/bin/warp-cli --accept-tos account)" == *"Missing"* ]]; then
+	/usr/bin/warp-cli --accept-tos register
+	/usr/bin/warp-cli --accept-tos enable-always-on
+fi
 /usr/bin/warp-cli --accept-tos connect
 /usr/sbin/iptables -t nat -A POSTROUTING -o CloudflareWARP -j MASQUERADE
 /usr/sbin/ip6tables -t nat -A POSTROUTING -o CloudflareWARP -j MASQUERADE
